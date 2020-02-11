26456bb403b6ce0039f513b0abb2400375084c2e
ADAM <zebolsky.adam@gmail.com>
2020-02-10 21:25:04 -0600

    made skills add and delete

--- js/main.js
+++ js/main.js
@@ -2,8 +2,11 @@ let $newLi = $('.new-skill');
 
 
 $('.btn-add').click(function(evt) {
-    $('ul').append($('<li>', {
-        text: $('$newLi').val()
-    }));
+    $('ul').append(`<li class="new-skill"><button class="btn btn-danger">X</button>${$newLi.val()}</li>`);
 });
 
+$('ul').on('click', 'button', function() {
+    $(this).closest('li').fadeOut(1000, function() {
+        $(this).remove();
+    });
+});
